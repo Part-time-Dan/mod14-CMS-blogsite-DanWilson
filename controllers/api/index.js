f22bfa0079906415c8560f2f313ddf4a7e5cc19c
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-25 11:59:39 -0600

    start handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 
-const projectRoutes = require('./blogRoutes');
+const blogRoutes = require('./blogRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', blogRoutes);
+router.use('/blogpost', blogRoutes);
 
 module.exports = router;
