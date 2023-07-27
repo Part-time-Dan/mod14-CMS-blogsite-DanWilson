1202c94ef383847f7d918eda97544037b907fa25
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-27 11:31:37 -0600

    new blog post up

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,34 +2,25 @@ const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// dashboard for logged in user
+// show user their own posts
 router.get('/', withAuth, (req, res) => {
-  Blog.findAll({
-    where: {
-      
-      user_id: req.session.user_id
-    },
-    attributes: [
-      'id',
-      'contents',
-      'title',
-      'created_at'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'content', 'blog_id', 'user_id'],
-        include: {
+    Blog.findAll({where: {user_id: req.session.user_id},
+      attributes: ['id','title','description','date_created'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'description', 'blog_id', 'user_id', 'date_created'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
           model: User,
           attributes: ['username']
         }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
+      ],
+    })
     .then(blogData => {
       const blogs = blogData.map(blog => blog.get({ plain: true }));
       res.render('dashboard', { blogs, logged_in: true });
@@ -39,5 +30,7 @@ router.get('/', withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
+
+
 
 module.exports = router;
