a8068c6fbce86b73c9bd0dcce46103bb2bae4429
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-27 23:40:05 -0600

    removed duplicate dashboard route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,36 +1,39 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
 
-// show user their own posts
-router.get('/', withAuth, (req, res) => {
-    Blog.findAll({where: {user_id: req.session.user_id},
-      
-      attributes: ['id','title','description','date_created'],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'description', 'blog_id', 'user_id', 'date_created'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+router.get('/', (req, res) => {
+  if (!req.session.logged_in) {
+    // If the user is not logged in, redirect to the 'choice' page
+    return res.redirect('/choice');
+  }
+  // If the user is logged in, render the dashboard template
+  Blog.findAll({
+    where: { user_id: req.session.user_id },
+    attributes: ['id', 'title', 'description', 'date_created'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'description', 'blog_id', 'user_id', 'date_created'],
+        include: {
           model: User,
-          attributes: ['username']
-        }
-      ],
-    })
-
-    .then(blogData => {
-      const blogs = blogData.map(blog => blog.get({ plain: true }));
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then((blogData) => {
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
       res.render('dashboard', { blogs, logged_in: true });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
 
 module.exports = router;
